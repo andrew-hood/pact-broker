f9ccff80e9b9c937b65be1676282f52846049508
Andrew Hood <ahood@go1.com>
2021-04-17 23:18:57 +1000

    fix lint

--- app/containers/mutations/createContainer.ts
+++ app/containers/mutations/createContainer.ts
@@ -10,7 +10,7 @@ const CreateContainer = z
 
 export default resolver.pipe(resolver.zod(CreateContainer), resolver.authorize(), async (input) => {
   // TODO: in multi-tenant app, you must add validation to ensure correct tenant
-  const container = await db.container.create({ data: input })
+  const container = await db.container.create({ data: input as any })
 
   return container
 })
